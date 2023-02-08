5fa76196f32b4ef50f0d65e56d18624e1f3f0901
Bhargav Ponnapalli <imbhargav5@gmail.com>
2023-02-09 02:31:55 +0530

    chore(api-routes): remove unused api routes

--- src/pages/api/hello.ts
+++
@@ -1,5 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function (req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json({ name: 'NextBase' });
-}
