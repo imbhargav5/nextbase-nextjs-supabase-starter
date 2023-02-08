c9cc9219817fd5f2c7a9e06172cc024cb4910f1e
Bhargav Ponnapalli <imbhargav5@gmail.com>
2023-02-08 21:33:10 +0530

    asdf

--- e2e/about.spec.ts
+++ e2e/about.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test'
 
-test('should have text in home page', async ({ page }) => {
+test('should have text in about page', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-  await page.goto('/')
-  await expect(page.locator('h1')).toContainText('Items')
+  await page.goto('/about')
+  await expect(page.locator('h1')).toContainText('About')
 })
