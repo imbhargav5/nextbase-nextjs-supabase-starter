0030eaab147b3f4184c42dca160d8030bb944cfe
Bhargav Ponnapalli <imbhargav5@gmail.com>
2023-02-09 02:05:31 +0530

    fix(react-no-ssr): remove react no ssr

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { Toaster } from 'react-hot-toast';
-import ReactNoSSR from 'react-no-ssr';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // Create a client
 const queryClient = new QueryClient();
@@ -9,9 +8,7 @@ export const ClientLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="max-w-lg mx-auto py-16">
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-      <ReactNoSSR>
-        <Toaster />
-      </ReactNoSSR>
+      <Toaster />
     </div>
   );
 };
