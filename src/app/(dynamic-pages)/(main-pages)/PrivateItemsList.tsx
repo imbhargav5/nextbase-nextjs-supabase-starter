1a31c60884370a05e07ee658d941264939147f1a
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-01-22 20:42:14 +0530

    feat(landing-page): improved the UI of item list and private items

--- src/app/(dynamic-pages)/(main-pages)/PrivateItemsList.tsx
+++ src/app/(dynamic-pages)/(main-pages)/PrivateItemsList.tsx
@@ -8,13 +8,13 @@ export const PrivateItemsList = ({
   privateItems: Table<'private_items'>[];
 }) => {
   return (
-    <div className="space-y-8">
+    <div className="space-y-8 py-8">
       <div className="flex justify-between items-baseline">
         <div className="space-y-2">
-          <h1 className="mt-1 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl lg:text-5xl">
+          <h1 className="mt-1 text-2xl font-thin tracking-tight text-gray-900 sm:text-4xl lg:text-5xl">
             Private Items
           </h1>
-          <p className="text-gray-600 text-sm italic">
+          <p className="text-gray-600 text-sm">
             These items can only be created by logged in users.
           </p>
         </div>
@@ -25,16 +25,18 @@ export const PrivateItemsList = ({
         </div>
       </div>
       {privateItems.length ? (
-        <div className="list-none space-y-2 m-0 pb-3 divide-y divide-gray-200 bg-white shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+        <div className="list-none m-0 divide-y divide-gray-200 bg-white shadow ring-1 ring-black ring-opacity-5">
           {privateItems.map((privateItem) => (
             <Link
               href={`/private-item/${privateItem.id}`}
-              className="px-3 block cursor-pointer pt-4 pb-3 text-left text-sm font-semibold text-gray-900"
+              className="px-3 block cursor-pointer pt-4 pb-3 text-left text-sm font-semibold text-gray-900 group hover:bg-blue-600 transition-all duration-200"
               key={privateItem.id}
             >
               <div className="space-y-2">
-                <p className="text-blue-600 text-lg">{privateItem.name}</p>
-                <p className="text-gray-600 text-sm">
+                <p className="text-blue-600 font-normal text-lg group-hover:text-white">
+                  {privateItem.name}
+                </p>
+                <p className="text-gray-600 font-medium text-sm group-hover:text-white">
                   {privateItem.description}
                 </p>
               </div>
