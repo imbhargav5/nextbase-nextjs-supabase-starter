a4dc245b05e292b44679bedbe04169a70de2e06d
Ponnapalli Bhargav <bhargavponnapalli.5@gmail.com>
2026-06-26 08:10:54 +0530

    chore(web): remove auth claims debug log

--- apps/web/src/rsc-data/supabase.ts
+++ apps/web/src/rsc-data/supabase.ts
@@ -39,10 +39,8 @@ export const getCachedLoggedInUserClaims = cache(async () => {
   return data.claims;
 });
 
-
 export const getCachedIsUserLoggedIn = cache(async () => {
   const claims = await getCachedLoggedInUserClaims();
-  console.log('claims', claims);
   return claims.sub !== null;
 });
 
