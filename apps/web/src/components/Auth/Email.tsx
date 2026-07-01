3bec38168e3573ccfaf64a9a512a465776117190
Ponnapalli Bhargav <imbhargav5@gmail.com>
2026-07-01 05:57:00 +0530

    chore(web): normalize auth email placeholder (#160)

--- apps/web/src/components/Auth/Email.tsx
+++ apps/web/src/components/Auth/Email.tsx
@@ -64,7 +64,7 @@ export const Email = ({
               disabled={isLoading}
               onChange={(event) => setEmail(event.target.value)}
               autoComplete={'email'}
-              placeholder="placeholder@email.com"
+              placeholder="email@example.com"
               required
             />
           </div>
